b94a67fc9148bdf5455f91d5404b1bcc4b0b5a32
karan240 <115027708+karan240@users.noreply.github.com>
2022-11-05 19:15:39 +0530

    Add files via upload

---
+++ netflix-clone/inde.js
@@ -0,0 +1,26 @@
+document.querySelector(".right1").addEventListener("click", function() {
+    document.querySelector(".list1").scrollBy(500, 0);
+    
+})
+document.querySelector(".left1").addEventListener("click", function() {
+    document.querySelector(".list1").scrollBy(-500, 0);
+})
+// ----------------------------------------------------------------------------
+document.querySelector(".right2").addEventListener("click", function() {
+    document.querySelector(".list2").scrollBy(500, 0);
+    
+    // alert("pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY
+})
+document.querySelector(".left2").addEventListener("click", function() {
+    document.querySelector(".list2").scrollBy(-500, 0);
+    // alert("pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY
+})
+document.querySelector(".right3").addEventListener("click", function() {
+    document.querySelector(".list3").scrollBy(500, 0);
+    
+    // alert("pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY
+})
+document.querySelector(".left3").addEventListener("click", function() {
+    document.querySelector(".list3").scrollBy(-500, 0);
+    // alert("pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY
+})
